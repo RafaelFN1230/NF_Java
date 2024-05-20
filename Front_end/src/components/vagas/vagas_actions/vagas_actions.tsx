c3e147a7a7464e0953a8244fc566fad52e627915
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-19 23:15:17 -0300

    Refactoring: Connecting new features to existing component

--- Front_end/src/components/vagas/vagas_actions/vagas_actions.tsx
+++ Front_end/src/components/vagas/vagas_actions/vagas_actions.tsx
@@ -1,24 +1,25 @@
 "use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Button } from "../../ui/button";
 import ActionEdit from "./vagas_editar";
 import ActionAddCandidate from "./vagas_add_candidato";
 import ActionDelete from "./vagas_excluir";
 
-export default function VagasActionButtons() {
+
+interface JobOffer {
+  id: number;
+  nome: string;
+  descricao: string;
+  salario: string;
+  onEditComplete: () => void
+}
+
+export default function VagasActionButtons({ id, nome, descricao, salario, onEditComplete }: JobOffer) {
   return (
     <div className="flex justify-row mx-3">
-      <ActionDelete />
-      <ActionEdit />
-      <ActionAddCandidate />
+      <ActionDelete id={id} nome={nome} onEditComplete={onEditComplete}/>
+      <ActionEdit id={id} nome={nome} descricao={descricao} salario={salario} onEditComplete={onEditComplete}/>
+      <ActionAddCandidate jobOfferId={id}/>
+      
     </div>
   );
 }
