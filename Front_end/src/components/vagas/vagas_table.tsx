9f26309d790d8e147b85363afd657beae1f28e9c
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-19 23:25:20 -0300

    Feat: Creates joboffer table

--- Front_end/src/components/vagas/vagas_table.tsx
+++ Front_end/src/components/vagas/vagas_table.tsx
@@ -10,27 +10,82 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import VagasActionButtons from "./vagas_actions/vagas_actions";
+import { Employee, JobOffers } from "@/models/employee.model";
+import FuncionariosAPI from "@/api/funcionarios.api";
+import { useState, useEffect, useCallback } from "react";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import React from "react";
+import { Dialog } from "@radix-ui/react-dialog";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "../ui/hover-card";
+import VagasListaCandidatos from "./vagas_lista_candidatos";
 
 export default function VagasTable() {
+  const { DetalhesFuncionario } = FuncionariosAPI();
+  const [data, setData] = useState<Employee | null>(null);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const result = await DetalhesFuncionario(2);
+      setData(result);
+    } catch (error) {
+      console.error("Erro ao buscar dados do funcionário:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const vagas: JobOffers[] | undefined = data?.vagas.sort(
+    (a, b) => a.id - b.id,
+  );
+
   return (
     <>
       <Table>
         <TableCaption>Lista de Vagas</TableCaption>
         <TableHeader>
           <TableRow>
+            <TableHead className="flex justify-center items-center">
+              ID da vaga
+            </TableHead>
             <TableHead>Nome da vaga</TableHead>
             <TableHead>Descrição</TableHead>
             <TableHead>Salário</TableHead>
-            <TableHead>Expira</TableHead>
             <TableHead>Ações</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell>
-              <VagasActionButtons />
-            </TableCell>
-          </TableRow>
+          {vagas?.map((vaga) => (
+            <TableRow key={vaga.id}>
+              <TableCell className="flex justify-center">{vaga.id}</TableCell>
+              <TableCell>
+                <VagasListaCandidatos jobOffer={vaga}  onEditComplete={fetchData}/>
+              </TableCell>
+              <TableCell>{vaga.descricao}</TableCell>
+              <TableCell>{vaga.salario}</TableCell>
+              <TableCell>
+                <VagasActionButtons
+                  id={vaga.id}
+                  nome={vaga.nome}
+                  descricao={vaga.descricao}
+                  salario={vaga.salario}
+                  onEditComplete={fetchData}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </>
