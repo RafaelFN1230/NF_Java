e26d76a6e1f75bb67cf4bb10dd06e92873565687
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-19 23:28:37 -0300

    Feat: connect dashboatd to the back end and and send data to card, mainchart and donut chart

--- Front_end/src/components/DashboadBody/dashboard_body.tsx
+++ Front_end/src/components/DashboadBody/dashboard_body.tsx
@@ -4,21 +4,39 @@ import * as React from "react";
 import CardComponent from "./Card/card";
 import PieChartcomponent from "./Tables/donut_chart";
 import MainChart from "./Tables/main_chart";
+import FuncionariosAPI from "@/api/funcionarios.api";
+import { useCallback, useEffect, useState } from "react";
+import { Employee } from "@/models/employee.model";
 
 export default function DashboadBody() {
-  const data = [{ comCandidato: 50, semCandidato: 3 }];
+  const {ListaDeFuncionarios} = FuncionariosAPI()
+  const [data, setData] = useState<Employee[] | null>(null);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const result = await ListaDeFuncionarios();
+      setData(result);
+    } catch (error) {
+      console.error("Erro ao buscar dados do funcionário:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col-reverse">
         <div className="h-1/2">
-          <MainChart />
+          <MainChart funcionarios={data}/>
         </div>
         <div className="h-1/2 flex flex-row mt-5">
           <div className="w-1/2">
-            <PieChartcomponent />
+            <PieChartcomponent funcionarios={data}/>
           </div>
           <div className="w-1/2">
-            <CardComponent />
+            <CardComponent funcionarios={data}/>
           </div>
         </div>
       </div>
