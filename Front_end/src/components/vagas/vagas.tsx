e2fef77dde6524f198e025983864510b4d6cbac7
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-19 23:25:50 -0300

    Refactorinh: Updates vagas layout

--- Front_end/src/components/vagas/vagas.tsx
+++ Front_end/src/components/vagas/vagas.tsx
@@ -1,11 +1,23 @@
 "use cliente";
 
+import FuncionariosAPI from "@/api/funcionarios.api";
 import VagasTable from "./vagas_table";
+import { useEffect, useState } from "react";
+import ButtonAddJobOffers from "./vagas_add_job_offer";
+import { Toaster } from "sonner";
 
 export default function VagasBody() {
+  
+
   return (
     <>
-      <VagasTable />
+    <div className="flex  justify-end mr-10 my-3">
+      <ButtonAddJobOffers/>
+      </div>
+      <div>
+      <VagasTable/>
+      </div>
+      <Toaster richColors closeButton />
     </>
   );
 }
