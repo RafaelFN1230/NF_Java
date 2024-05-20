13bf2e361cf38f31d1f3cf5f1b850da27becaff3
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-19 23:19:37 -0300

    Feat: Crates Component that uses both delete and edit candidate componet

---
+++ Front_end/src/components/vagas/candidatos_actions/candidatos_actions.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import ActionEdit from "./candidatos_editar";
+import ActionDelete from "./candidatos_excluir";
+
+interface JobOffer {
+  id: number;
+  nome: string;
+  rg: string;
+  email: string;
+  resumoCurriculo: string;
+  onEditComplete: () => void
+}
+
+export default function CandidatosActionButtons({ id, nome, rg, email, resumoCurriculo, onEditComplete }: JobOffer) {
+  return (
+    <div className="flex justify-row mx-3">
+      <ActionDelete rg={rg} nome={nome} onEditComplete={onEditComplete}/>
+      <ActionEdit id={id} nome={nome} rg={rg} email={email} resumoCurriculo={resumoCurriculo} onEditComplete={onEditComplete}/>      
+    </div>
+  );
+}
