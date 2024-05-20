30902cdc814b8397a5fe45dd5142d6f0f0ec044f
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-19 23:13:55 -0300

    Feat: Crates feature of edit job offer

--- Front_end/src/components/vagas/vagas_actions/vagas_editar.tsx
+++ Front_end/src/components/vagas/vagas_actions/vagas_editar.tsx
@@ -9,15 +9,106 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "../../ui/button";
+import { FormProvider } from "react-hook-form";
+import InputFormField from "../forms/input/forms_field_input";
+import { useEffect, useState } from "react";
+import { InputTextAreaFormField } from "../forms/inputTextArea/forms_field_inputTextArea";
+import EditarVagaViewModel from "@/viewModel/EditarVaga.ViewModel";
+import { toast } from "sonner";
+import { Form } from "@/components/ui/form";
 
-export default function ActionEdit() {
+
+
+interface ActionEditProps {
+  id: number;
+  nome: string;
+  descricao: string;
+  salario: string;
+  onEditComplete: () => void; 
+}
+interface JobOffer {
+  id: number;
+  nome: string;
+  descricao: string;
+  salario: string;
+}
+interface JobOfferForm {
+  nome: string;
+  descricao: string;
+  salario: string;
+}
+
+export default function ActionEdit({ id, nome, descricao, salario, onEditComplete }: ActionEditProps) {
+  const { EditJobOfferForm, errorMessage, setJobOffer } = EditarVagaViewModel();
+  const { form, onSubmit } = EditJobOfferForm();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setJobOffer({ id, nome, descricao, salario });
+  }, [id, nome, descricao, salario, setJobOffer]);
+
+  const handleSubmit = async (data: JobOfferForm) => {
+    const result = await onSubmit(data);
+    if (result.success) {
+      setIsOpen(false);
+      toast.info("Vaga editada com sucesso!", {description: `ID da vaga:   ${id} Nome da vaga:   ${data.nome}      Descricao:     ${data.descricao}      Salário: ${data.salario}` , duration: 8000 })
+      onEditComplete()
+    }
+  };
   return (
     <>
-      <Dialog>
-        <DialogTrigger>
-          <Button className="bg-blue-500 text-white mx-3">Editar</Button>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <Button className="bg-blue-500 text-white mx-3 hover:bg-blue-700">Editar</Button>
         </DialogTrigger>
-        <DialogContent>Editar</DialogContent>
+        <DialogContent>
+        <DialogHeader>
+            <DialogTitle>Editar Vaga</DialogTitle>
+            <DialogDescription>
+              Insira novas informações da vaga.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form} >
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="bg-white rounded px-8 pt-6 mb-4 h-full"
+            >
+              <InputFormField
+                name={"nome"}
+                text={"Nome"}
+                placeholder={""}
+                type={"nome"}
+                form={form}
+              />
+              <InputFormField
+                  name={"salario"}
+                  text={"Salário"}
+                  placeholder={""}
+                  type={"salario"}
+                  form={form}
+                />
+              <InputTextAreaFormField
+                name={"descricao"}
+                text={"Descrição"}
+                placeholder={""}
+                type={"descricao"}
+                form={form}
+              />
+              <div className="w-full flex justify-center">
+              <Button type="submit" className="mt-5 px-10 bg-blue-500 text-white hover:bg-blue-700">
+                Editar
+              </Button>
+              </div>
+              <div className="w-full max-w-xs">
+                {errorMessage && (
+                  <p className="font-bold text-sm text-red-500">
+                    {errorMessage}
+                  </p>
+                )}
+              </div>
+            </form>
+          </Form>
+        </DialogContent>
       </Dialog>
     </>
   );
