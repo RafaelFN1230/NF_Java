521cacf39cc15a4ce8a23673721e49046e119d23
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-29 23:55:12 -0300

    Feat: Adds toater to login

--- Front_end/src/view/login.tsx
+++ Front_end/src/view/login.tsx
@@ -1,10 +1,12 @@
 import { ReactElement } from "react";
 import Login from "@/components/login/login";
+import { Toaster } from "sonner";
 
 export function LoginComponents(): ReactElement {
   return (
     <div className="flex justify-center items-center h-screen">
       <Login />
+      <Toaster richColors closeButton />
     </div>
   );
 }
