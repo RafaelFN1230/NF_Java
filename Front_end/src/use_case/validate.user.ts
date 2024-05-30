ded90ab9244d9c1935234e21ae95acd574a69290
RafaelFN1230 <rafaelfnascimento1230@gmail.com>
2024-05-30 00:10:42 -0300

    Feat: Create a use case to validade user

---
+++ Front_end/src/use_case/validate.user.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import { ValidateUser } from '../repositories/repositories';
+import { createCookies } from '@/actions/cookies';
+
+export const validateUserUsecase = async (email: string, password: string) => {
+  try {
+    const response = await ValidateUser({
+      email: email,
+      senha: password
+    });
+    console.log("response: ", response)
+    if (response){
+      await createCookies('user_id', response.id);
+      return 'authorized';
+    }
+  } catch (error: any) {
+    if (error.response) {
+      if (error.response.status === 401) {
+        return 'unauthorized';
+      } else if (error.response.status === 500) {
+        return 'server-error';
+      }
+    }
+    return 'server-error';
+  }
+};
